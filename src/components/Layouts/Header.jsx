22ae25b91ccf17430c83fc4c37fa4c6045151d33
Rakibmahmud <hasansaikat74@gmail.com>
2023-07-20 23:25:16 +0600

    added banner

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-cyan-600 shadow-2xl">
+            <div className="navbar bg-cyan-600 shadow-2xl mx-auto">
                 <div className="flex-1">
                     <h3 className="btn btn-ghost normal-case text-3xl text-gray-900">
                         korean<span className='text-rose-400 text-4xl'>CHEF</span></h3>
